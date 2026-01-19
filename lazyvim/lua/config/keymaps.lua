0b689741951ecd861c71167e967dfb77ddc51224
conglt10 <conglt1712@gmail.com>
2026-01-19 20:30:41 +0700

    edit mapping for pop ai agent

--- lazyvim/lua/config/keymaps.lua
+++ lazyvim/lua/config/keymaps.lua
@@ -1,4 +1,33 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set('n', '<leader>cp', '<cmd>CpRel<CR>', { desc = 'Copy relative path' })
+local map = vim.keymap.set
+
+map('n', '<leader>cp', '<cmd>CpRel<CR>', { desc = 'Copy relative path' })
+
+local function open_copilot_term()
+  local width = math.floor(vim.o.columns * 0.33)
+  vim.cmd("vsplit")
+  vim.cmd("vertical resize " .. width)
+  vim.cmd("term copilot")
+  vim.cmd("startinsert")
+end
+
+local function open_gemini_term()
+  local width = math.floor(vim.o.columns * 0.33)
+  vim.cmd("vsplit")
+  vim.cmd("vertical resize " .. width)
+  vim.cmd("term gemini")
+  vim.cmd("startinsert")
+end
+
+local function open_opencode_term()
+  local width = math.floor(vim.o.columns * 0.33)
+  vim.cmd("vsplit")
+  vim.cmd("vertical resize " .. width)
+  vim.cmd("term opencode")
+  vim.cmd("startinsert")
+end
+
+map('n', '<M-b>', open_opencode_term, { desc = "Open opencode terminal (1/3 width)" })
+map('n', '<M-l>', open_copilot_term, { desc = "Open copilot terminal (1/3 width)" })
+map('n', '<C-g>', open_gemini_term, { desc = "Open gemini terminal (1/3 width)" })
+map('t', '<M-w>', [[<C-\><C-n><cmd>q!<CR>]], { desc = "Exit split view" })
+
--- nvchad/lua/mappings.lua
+++ nvchad/lua/mappings.lua
@@ -24,6 +24,14 @@ local function open_copilot_term()
   vim.cmd("startinsert")
 end
 
+local function open_gemini_term()
+  local width = math.floor(vim.o.columns * 0.33)
+  vim.cmd("vsplit")
+  vim.cmd("vertical resize " .. width)
+  vim.cmd("term gemini")
+  vim.cmd("startinsert")
+end
+
 local function open_opencode_term()
   local width = math.floor(vim.o.columns * 0.33)
   vim.cmd("vsplit")
@@ -32,6 +40,7 @@ local function open_opencode_term()
   vim.cmd("startinsert")
 end
 
-map('n', '<C-b>', open_opencode_term, { desc = "Open opencode terminal (1/3 width)" })
-map('n', '<C-m>', open_copilot_term, { desc = "Open copilot terminal (1/3 width)" })
-map('t', '<C-w>', [[<C-\><C-n><cmd>q!<CR>]], { desc = "Exit split view" })
+map('n', '<M-b>', open_opencode_term, { desc = "Open opencode terminal (1/3 width)" })
+map('n', '<M-l>', open_copilot_term, { desc = "Open copilot terminal (1/3 width)" })
+map('n', '<C-g>', open_gemini_term, { desc = "Open gemini terminal (1/3 width)" })
+map('t', '<M-w>', [[<C-\><C-n><cmd>q!<CR>]], { desc = "Exit split view" })
